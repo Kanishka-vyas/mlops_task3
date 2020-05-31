0de9cd0faed6108fa1e5dcae124de93767376845
Kanishka-vyas <kanishkavyas74@gmail.com>
2020-05-31 19:58:40 +0530

    mlops3

---
+++ verification_mail.py
@@ -0,0 +1,13 @@
+import smtplib
+
+s = smtplib.SMTP("smtp.gmail.com" , 587)
+
+s.starttls()
+
+s.login("kanishkavyas74@gmail.com","josephedwardroot74")
+
+message = "your model has achieved desired accuracy"
+
+s.sendmail("kanishkavyas74@gmail.com","kanishkavyas74@gmail.com" , message)
+
+s.quit()
